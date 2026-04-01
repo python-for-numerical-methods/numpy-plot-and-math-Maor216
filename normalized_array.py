00d083639ff0699188c172c7cecebb1ee3b6c727
Maor216 <mistriel18@gmail.com>
2026-04-01 18:21:37 +0300

    Update normalized_array.py

--- normalized_array.py
+++ normalized_array.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def normalized_array(arr):
     min_val = np.min(arr)
-    max_val = np.max(arr)
+    max_val = np.max(arr
     if max_val == min_val:
         return np.zeros_like(arr, dtype=float)
     x_norm = (arr - min_val) / (max_val - min_val)
